b1ed2663ac283d8f26ea6dd2b8e0ad9e5c004a4a
Apprentice <apprentice@devbootcamp.com>
2013-03-13 17:00:53 -0700

    error handling and model validation

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,13 +1,17 @@
 get '/' do
   @urls = Url.order("id DESC").limit(3)
   @url = Url.where('short_url = ?', params[:s]).first
+  @error = params[:error]
   erb :index
 end
 
 post '/urls' do
-  url = Url.create(:long_url => params[:url], :click_count => 0)
-  # create a new Url
-  redirect "/?s=#{url.short_url}"
+  url = Url.new(:long_url => params[:url], :click_count => 0)
+  if url.save
+    redirect "/?s=#{url.short_url}"
+  else
+    redirect "/?error=Invalid URL"
+  end
 end
 
 # e.g., /q6bda
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,6 +1,9 @@
 class Url < ActiveRecord::Base
   before_create :create_short_url
 
+  validates :long_url, :format => { :with => /^[^\.][\w\.\-\/\:]{0,}\.\w{2,7}(\/|#|$)/,
+    :message => "Please input a valid url."}
+
   def create_short_url
     url = [('a'..'z').to_a,(0..9).to_a].flatten.shuffle[0...4].join('')
     return self.short_url = url unless Url.where('short_url = ?', url).count > 0
